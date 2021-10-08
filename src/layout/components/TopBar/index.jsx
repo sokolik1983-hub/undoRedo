f537e84018f3fc4d7892c542679f3b9ec9d559b0
Artem Arakelyan <aarakelyan@kameleoon.com>
2021-10-09 00:28:15 +0300

    add loading indicator

--- src/layout/components/TopBar/index.jsx
+++ src/layout/components/TopBar/index.jsx
@@ -3,12 +3,14 @@ import UserActions from "./UserActions";
 import styles from "./TopBar.module.scss";
 import Logo from "./Logo";
 import PageDescription from "./PageDescription";
+import LoadingIndicator from "./LoadingIndicator";
 
 function TopBar() {
   return (
     <header className={styles.header}>
       <Logo />
       <PageDescription />
+      <LoadingIndicator />
       <UserActions />
     </header>
   );
