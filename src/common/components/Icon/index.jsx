aa8478b88f12202342c0f021e091f48f09d4e6bf
Dmitriy Nichiporenko <C:\The Bat!>
2021-10-18 18:50:31 +0300

    icon

---
+++ src/common/components/Icon/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import styles from './Icon.module.scss';
+
+/**
+ * @param size - строка,размер иконки, один из small, medium, large, по умолчанию medium
+ * @param onSelectItem - строка, внешний класс для стилизации иконки
+ * @param children - иконка, которую отрисует компонент Icon
+ */
+
+const Icon = ({ size, className, children, ...props }) => {
+  const classes = clsx(
+    className,
+    { [styles.small]: size === 'small' },
+    { [styles.medium]: size === 'medium' },
+    { [styles.large]: size === 'large' }
+  );
+
+  return (
+    <div className={classes} {...props}>
+      {children}
+    </div>
+  );
+};
+
+export default Icon;
+
+Icon.propTypes = {
+  size: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.node
+};
+
+Icon.defaultProps = {
+  size: 'medium',
+  className: '', 
+  children: null
+};
