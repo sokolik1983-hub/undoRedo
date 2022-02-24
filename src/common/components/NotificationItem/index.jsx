89593fb08c82257797b7554f955cb66488983f30
Igor Levin <levin.igor@bk.ru>
2022-02-24 19:29:49 +0300

    TU2-48: Разработка уведомлений от системы

--- src/common/components/NotificationItem/index.jsx
+++ src/common/components/NotificationItem/index.jsx
@@ -1,30 +1,12 @@
 import cn from 'clsx';
 import PropTypes from 'prop-types';
-// import { useEffect, useState } from 'react';
 import WarnIcon from '../../../layout/assets/warnIcon.svg';
 import styles from './NotificationItem.module.scss';
-
-// const AUTO_HIDE_DURATION = 5000;
 
 const NotificationItem = ({
   notification: { id, autoHide, variant, title, message, buttonText },
   onClose
 }) => {
-  // const [closeTimer, setCloseTimer] = useState(null);
-  //
-  // useEffect(() => {
-  //   if (autoHide) {
-  //     setCloseTimer(
-  //       setTimeout(() => {
-  //         onClose(id);
-  //       }, AUTO_HIDE_DURATION)
-  //     );
-  //   }
-  //
-  //   return () => {
-  //     clearTimeout(closeTimer);
-  //   };
-  // }, []);
 
   const handleAlertClose = () => onClose(id);
 
