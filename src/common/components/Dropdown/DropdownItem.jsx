bde6e703765750d5ee9166f28b2f9a26d44c3a17
Artem Arakelyan <aarakelyan@kameleoon.com>
2022-03-02 14:37:05 +0300

    refactor dropdown

--- src/common/components/Dropdown/DropdownItem.jsx
+++ src/common/components/Dropdown/DropdownItem.jsx
@@ -1,36 +1,38 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import cn from 'clsx';
 import styles from './Dropdown.module.scss';
 
-const DropdownItem = ({ className, onClick, title, icon, children, text }) => {
+const DropdownItem = ({ className, onClick, children, item }) => {
+  const { title, action, text, icon } = item;
+
+  const handleClick = () => {
+    onClick(action);
+  };
 
   return (
     <div
       className={cn(styles.dropDownItem, className)}
-      onClick={(e) => onClick(e)}
+      onClick={handleClick}
       title={title}
     >
-      { icon && (
-        <div className={styles.icon}>
-          { icon }
-        </div>
-      ) }
-      { children || text }
+      {icon && <div className={styles.icon}>{icon}</div>}
+      {children || text}
     </div>
-  )
+  );
 };
 
 export default DropdownItem;
 
-DropdownItem.prototype = {
-
-}
+DropdownItem.prototype = {};
 
 DropdownItem.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
-  icon: PropTypes.node,
-  onClick: PropTypes.func.isRequired,
-  text: PropTypes.string,
-  title: PropTypes.string.isRequired
-}
+  item: PropTypes.objectOf({
+    icon: PropTypes.node,
+    text: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    action: PropTypes.string
+  }),
+  onClick: PropTypes.func.isRequired
+};
--- src/pages/HomePage/UserMenu/index.jsx
+++ src/pages/HomePage/UserMenu/index.jsx
@@ -3,7 +3,10 @@ import { useDispatch } from 'react-redux';
 import Dropdown from '../../../common/components/Dropdown';
 import DropdownItem from '../../../common/components/Dropdown/DropdownItem';
 import { logoutUser } from '../../../data/actions/auth';
-import { REDIRECT_LINKS, DEFAULT_USER_ACTIONS } from '../../../common/constants/common';
+import {
+  REDIRECT_LINKS,
+  DEFAULT_USER_ACTIONS
+} from '../../../common/constants/common';
 import AvatarIcon from '../../../layout/assets/miniAvatar.svg';
 import styles from './UserMenu.module.scss';
 
@@ -11,7 +14,6 @@ const UserMenu = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const userInfo = window.localStorage.getItem('userInfo');
-
 
   const mainButton = () => {
     return (
@@ -22,9 +24,8 @@ const UserMenu = () => {
         <div className={styles.mainButtonRight}>
           <span>{JSON.parse(userInfo).user_login}</span>
         </div>
-
       </div>
-    )
+    );
   };
 
   const handleLogoutClick = () => {
@@ -33,7 +34,7 @@ const UserMenu = () => {
     return false;
   };
 
-  const handleItemClick = (action) => {
+  const handleItemClick = action => {
     switch (action) {
       case 'logout':
         handleLogoutClick();
@@ -44,16 +45,22 @@ const UserMenu = () => {
   };
 
   const makingItems = () => {
-   return DEFAULT_USER_ACTIONS.map(item => {
-     return <DropdownItem onClick={() => handleItemClick(item.action)} className={styles.menuItem} title={item.title} text={item.text} />
-   })
+    return DEFAULT_USER_ACTIONS.map(item => {
+      return (
+        <DropdownItem
+          onClick={handleItemClick}
+          className={styles.menuItem}
+          item={item}
+        />
+      );
+    });
   };
 
   return (
     <Dropdown mainButton={mainButton()} itemsWrapper={styles.itemsWrapper}>
       {makingItems()}
     </Dropdown>
-  )
+  );
 };
 
 export default UserMenu;
