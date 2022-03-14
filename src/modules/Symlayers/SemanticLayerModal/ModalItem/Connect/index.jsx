585a8662287797b57c27025563197603ff522f55
Elena Podtikan <C:\The Bat!>
2022-03-14 22:13:18 +0300

    add business logic to semanticLayerModal

--- src/modules/Symlayers/SemanticLayerModal/ModalItem/Connect/index.jsx
+++ src/modules/Symlayers/SemanticLayerModal/ModalItem/Connect/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Gears from '../../../../../common/components/Gears';
 import ModalItem from '..';
 import styles from './Connect.module.scss';
-import { ReactComponent as GearsIcon } from '../../../../../layout/assets/semanticLayerModal/gears.svg';
 import { ReactComponent as WireIcon } from '../../../../../layout/assets/semanticLayerModal/wire.svg';
 import Select from '../../../../../common/components/Select';
 import Button from '../../../../../common/components/Button';
 
-const Connect = () => {
+/**
+ * @param title - строка для заголовка
+ */
+
+const Connect = ({title}) => {
+
   return (
-    <ModalItem title='Cоединение'>
+    <ModalItem title={title}>
       <div className={styles.wrapper}>
         <div className={styles.iconWrapper}>
           <div>
@@ -16,10 +22,14 @@ const Connect = () => {
             <div className={styles.hide}>
               <p className={styles.text}>создать соединение</p>
             </div>
+            <Gears isSpinning className={styles.gearsIcon} />
           </div>
-          <GearsIcon className={styles.gearsIcon} />
         </div>
-        <Select />
+        <Select 
+          defaultValue='Коннектор 01 проба'
+          name='name1' 
+          options={[{value: '1', text: 'Коннектор 02 проба'}]} 
+        />
         <div className={styles.buttonsWrapper}>
           <Button className={styles.edit}>
             Редактировать
@@ -34,3 +44,11 @@ const Connect = () => {
 };
 
 export default Connect;
+
+Connect.propTypes = {
+  title: PropTypes.string
+};
+
+Connect.defaultProps = {
+  title: ''
+};
--- src/modules/Symlayers/SemanticLayerModal/ModalItem/SqlItem/index.jsx
+++ src/modules/Symlayers/SemanticLayerModal/ModalItem/SqlItem/index.jsx
@@ -1,50 +1,78 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ModalItem from '..';
 import styles from './SqlItem.module.scss';
-import CheckBox from '../../../../../common/components/CheckBox';
-import Radio from '../../../../../common/components/Radio';
+import CheckboxField from '../../../../../common/components/formikFields/checkboxField';
+import RadioField from '../../../../../common/components/formikFields/radioField';
 
-const SqlItem = () => {
+/**
+ * @param title - строка для заголовка
+ */
+
+const SQLRequestMap = [
+  { value: 'semirequests', label: 'Подзапросы' },
+  { value: 'operators', label: 'Операторы объединения, пересечения и исключения' },
+  { value: 'hardOperators', label: 'Сложные операторы в панели запросов' },
+];
+
+const SQLMultipleRoads = [
+  { value: 'context', label: 'Множественные запросы в одном контексте' },
+  { value: 'measure', label: 'Множественные запросы для каждой меры' },
+  { value: 'miltiContext', label: 'Выбор нескольких контексов' },
+];
+
+const CartesianWork = [
+  { value: 'notAllow', label: 'Не позволять' },
+  { value: 'warn', label: 'Предупреждать' }
+]
+
+const SqlItem = ({title}) => {
   return (
-    <ModalItem title='SQL'>
+    <ModalItem title={title}>
       <div className={styles.wrapperColumn}>
         <div className={styles.wrapper}>
           <div className={styles.wrapperColumn}>
             <p>Запрос</p>
             <div>
-              <div className={styles.textWrapper}>
-                <CheckBox />  
-                <p className={styles.text}>Подзапросы</p>
-              </div>  
-              <div className={styles.textWrapper}>
-                <CheckBox />
-                <p className={styles.text}>Операторы объединения, пересечения и исключения</p>
-              </div>
-              <div className={styles.textWrapper}>
-                <CheckBox />
-                <p className={styles.text}>Сложные операторы в панели запросов</p>
-              </div>
+              {SQLRequestMap.map(item => (
+                <CheckboxField
+                  name='SQLRequest'
+                  key={item.value} 
+                  labelClass={styles.label}
+                  wrapperClass={styles.checkbox}
+                  {...item}
+                />
+              ))}
             </div>
           </div>
           <div className={styles.indents}>
             <p>Множественные пути</p>
-            <div className={styles.textWrapper}>
-              <CheckBox />
-              <p className={styles.text}>Множественные запросы в одном контексте</p>
-            </div>
-            <div className={styles.textWrapper}>
-              <CheckBox />
-              <p className={styles.text}>Множественные запросы для каждой меры</p>
-            </div>
-            <div className={styles.textWrapper}>
-              <CheckBox />
-              <p className={styles.text}>Выбор нескольких контексов</p>
-            </div>
+            {SQLMultipleRoads.map(item => (
+              <CheckboxField
+                name="SQLMultipleRoads" 
+                key={item.value}
+                labelClass={styles.label}
+                wrapperClass={styles.checkbox}
+                {...item}
+              />
+            ))}
           </div>
         </div>
         <div>
           <p>Картезианское произведение</p>
-          <Radio text1='Не позволять' text2='Предупреждать' />
+          <div className={styles.radioWrapper}>
+            {CartesianWork.map((item, index) => {
+              const className = index === 0 ? styles.radioWithIndent : '';
+              return (
+                <RadioField
+                  name="CartesianWork"
+                  key={item.value}
+                  wrapperClass={className}
+                  {...item}
+                />
+              )
+            })}
+          </div>
         </div>
       </div>
     </ModalItem>
@@ -52,3 +80,11 @@ const SqlItem = () => {
 };
 
 export default SqlItem;
+
+SqlItem.propTypes = {
+  title: PropTypes.string
+};
+
+SqlItem.defaultProps = {
+  title: ''
+};
