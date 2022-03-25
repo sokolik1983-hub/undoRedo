32e4170608bf3168212f3237dea9e238175e8947
Ivan Prohorov <C:\Users\ivanp\AppData\Roaming\The Bat!>
2022-03-25 18:06:03 +0300

    friday evening

--- src/modules/QueryPanel/PromptPropertiesLayer/index.jsx
+++ src/modules/QueryPanel/PromptPropertiesLayer/index.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
 // import modalStyles from '../../Symlayers/SemanticLayerModal/SemanticLayerModal.module.scss';
-
+import Button from '../../../common/components/Button';
 import modalStyles from './PromptProperties.module.scss';
 import styles from '../QueryPanel.module.scss';
 import Modal from '../../../common/components/Modal';
 import Radio from '../../../common/components/Radio';
 import CheckBox from '../../../common/components/CheckBox';
+import { BUTTON } from '../../../common/constants/common';
+import { ReactComponent as Arrow } from '../../../layout/assets/queryPanel/arrowBold.svg';
 
 const PromptPropertiesLayer = ({ visible, onClose }) => {
   const closeHandler = () => {
@@ -19,12 +21,22 @@ const PromptPropertiesLayer = ({ visible, onClose }) => {
           <p className={modalStyles.text}>Тип параметра</p>
 
           <div className={modalStyles.allRadio}>
-            <Radio />
-            <p className={modalStyles.text}>Новая подсказка</p>
+            <Radio
+              id="newPrompt"
+              name="paramTypeRadio"
+              label="Новая подсказка"
+              value="newPrompt"
+              labelClass={modalStyles.label}
+            />
           </div>
           <div className={modalStyles.allRadio}>
-            <Radio />
-            <p className={modalStyles.text}>Параметр юниверса</p>
+            <Radio
+              id="universeParam"
+              name="paramTypeRadio"
+              label="Параметр Юниверса"
+              value="universeParam"
+              labelClass={modalStyles.label}
+            />
           </div>
         </div>
 
@@ -38,28 +50,77 @@ const PromptPropertiesLayer = ({ visible, onClose }) => {
 
         <div className={modalStyles.allCheckBoxBlock}>
           <div className={modalStyles.checkBox}>
-            <CheckBox name="promptWithValueList" />
-            <p className={modalStyles.text}>Подсказка со списком значений</p>
+            <CheckBox
+              id="listValueBox"
+              name="listValueBox"
+              label="Подсказка со списком значений"
+              value="listValueBox"
+              labelClass={modalStyles.label}
+            />
           </div>
-
           <div className={modalStyles.checkBox}>
-            <CheckBox name="onlyListChoice" />
-            <p className={modalStyles.text}>Выбор только из списка</p>
+            <CheckBox
+              id="onlyListChoiceBox"
+              name="onlyListChoiceBox"
+              label="Выбор только из списка"
+              value="onlyListChoiceBox"
+              labelClass={modalStyles.label}
+            />
           </div>
-
           <div className={modalStyles.checkBox}>
-            <CheckBox name="lastChosenValues" />
-            <p className={modalStyles.text}>Последние выбранные значения</p>
+            <CheckBox
+              id="lastChosenValuesBox"
+              name="lastChosenValuesBox"
+              label="Последние выбранные значения"
+              value="lastChosenValuesBox"
+              labelClass={modalStyles.label}
+            />
           </div>
-
           <div className={modalStyles.checkBox}>
-            <CheckBox name="additionalPrompt" />
-            <p className={modalStyles.text}>Дополнительная подсказка</p>
+            <CheckBox
+              id="additionalPromptBox"
+              name="additionalPromptBox"
+              label="Дополнительная подсказка"
+              value="additionalPromptBox"
+              labelClass={modalStyles.label}
+            />
           </div>
         </div>
 
         <div className={modalStyles.byDefaultBlock}>
           <p className={modalStyles.text}>По умолчанию</p>
+
+          <div className={modalStyles.defaultCheckBoxButton}>
+            <div className={modalStyles.defaultCheckBox}>
+              <CheckBox
+                id="defaultValuesBox"
+                name="defaultValuesBox"
+                label="Значения по умолчанию"
+                value="defaultValuesBox"
+                labelClass={modalStyles.label}
+              />
+            </div>
+            <Button className={modalStyles.button} buttonStyle={BUTTON.BLUE}>
+              Список
+            </Button>
+          </div>
+          <div className={modalStyles.inputArrowBlock}>
+            <input className={modalStyles.input} />
+            <Arrow className={modalStyles.arrow} />
+            <div className={modalStyles.dataWindow} />
+          </div>
+        </div>
+
+        <div className={modalStyles.footerButtons}>
+          <Button
+            className={modalStyles.button}
+            buttonStyle={BUTTON.BIG_ORANGE}
+          >
+            Сохранить
+          </Button>
+          <Button className={modalStyles.button} buttonStyle={BUTTON.BIG_BLUE}>
+            Отмента
+          </Button>
         </div>
       </div>
     );
