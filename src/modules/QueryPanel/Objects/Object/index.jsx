479c3279a85d28c8d36029c7332ab95e9ea8b75d
Azim Makaev <C:\Users\azimm\AppData\Roaming\The Bat!>
2022-03-28 16:44:08 +0300

    добавлена вертска объектов и возможность эти объекты удалять

---
+++ src/modules/QueryPanel/Objects/Object/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Object.module.scss';
+import { ReactComponent as BlackCross} from '../../../../layout/assets/closeBlack.svg' 
+// import { ReactComponent as OrangeIcon } from '../../../layout/assets/queryPanel/orangeIcon.svg';
+import { ReactComponent as GreenIcon } from '../../../../layout/assets/queryPanel/greenIcon.svg';
+// import { ReactComponent as BlueIcon } from '../../../layout/assets/queryPanel/blueIcon.svg';
+
+const ObjectItem = ({id, title, type, onDeleteObjItem}) => {
+  
+  const handleDelete = () => {
+    onDeleteObjItem(id);
+  }
+
+  const chooseIcon = () => {
+    switch (type) {
+      case 'green':
+        return <GreenIcon />
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className={styles.wrapper} id={`object-${id}`}>
+      <div>
+        {chooseIcon(type)}
+      </div>
+      <span>
+        {title.length > 16 ? `${title.substring(0, 16)}...` : title}
+      </span>
+      <button type='button' onClick={handleDelete}>
+        <BlackCross />    
+      </button>
+    </div>
+  );
+};
+
+export default ObjectItem;
+
+ObjectItem.propTypes = {
+  id: PropTypes.number,
+  title: PropTypes.string,
+  type: PropTypes.string,
+  onDeleteObjItem: PropTypes.func
+};
--- src/modules/QueryPanel/Objects/index.jsx
+++ src/modules/QueryPanel/Objects/index.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Divider from '../../../common/components/Divider';
 import styles from './Objects.module.scss';
 import { ReactComponent as Filter } from '../../../layout/assets/queryPanel/filter.svg';
 import { ReactComponent as Lists } from '../../../layout/assets/queryPanel/lists.svg';
 import { ReactComponent as Basket } from '../../../layout/assets/queryPanel/basket.svg';
+import ObjectItem from './Object/index';
 
 const Objects = ({ title }) => {
+  const objItem = {
+    id: 1,
+    title: 'Тестировка 200',
+    type: 'green' 
+  }
+  const objItem2 = {
+    id: 2,
+    title: 'Банковские проводки',
+    type: 'green'
+  }
+  const objItem3 = {
+    id: 3,
+    title:'Опа 200',
+    type: 'green'
+  }
+  const [objArr, setObjArr] = useState(null);
+
+  useEffect(() => {
+    setObjArr([objItem, objItem2, objItem3]);
+  }, []);
+
+  const onDeleteObjItem = (id) => {
+    setObjArr(objArr.filter(item => item.id !== id));
+  }
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.top}>
@@ -18,6 +44,9 @@ const Objects = ({ title }) => {
         </div>
       </div>
       <Divider color='#FFFFFF' />
+      <div className={styles.objectList}>
+        {objArr?.map(obj => <ObjectItem id={obj.id} title={obj.title} type={obj.type} onDeleteObjItem={onDeleteObjItem} />)}
+      </div>
     </div>
   );
 };
