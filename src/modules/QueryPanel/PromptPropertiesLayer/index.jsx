09eec35694d588f5411653df84634db642ddd7d8
Ivan Prohorov <C:\Users\ivanp\AppData\Roaming\The Bat!>
2022-03-29 13:44:42 +0300

    task completed

--- src/modules/QueryPanel/PromptPropertiesLayer/index.jsx
+++ src/modules/QueryPanel/PromptPropertiesLayer/index.jsx
@@ -1,128 +1,125 @@
 import PropTypes from 'prop-types';
-// import modalStyles from '../../Symlayers/SemanticLayerModal/SemanticLayerModal.module.scss';
-import Button from '../../../common/components/Button';
-import modalStyles from './PromptProperties.module.scss';
-import styles from '../QueryPanel.module.scss';
-import Modal from '../../../common/components/Modal';
-import Radio from '../../../common/components/Radio';
-import CheckBox from '../../../common/components/CheckBox';
+import { Formik } from 'formik';
+
 import { BUTTON } from '../../../common/constants/common';
+import Button from '../../../common/components/Button';
+import Modal from '../../../common/components/Modal';
+import RadioField from '../../../common/components/formikFields/radioField';
+// import CheckBox from '../../../common/components/CheckBox';
 import { ReactComponent as Arrow } from '../../../layout/assets/queryPanel/arrowBold.svg';
+
+import styles from '../QueryPanel.module.scss';
+import modalStyles from './PromptProperties.module.scss';
+import CheckboxField from '../../../common/components/formikFields/checkboxField';
 
 const PromptPropertiesLayer = ({ visible, onClose }) => {
   const closeHandler = () => {
     return onClose();
   };
 
+  const PromptValuesRadioBTNs = [
+    { value: 'newPrompt', label: 'Новая подсказка' },
+    { value: 'paramTypeRadio', label: 'Параметр юниверса' }
+  ];
+
+  const CheckBoxValues = [
+    { value: 'listValueBoxt', label: 'Подсказка со списком значений' },
+    { value: 'onlyListChoiceBox', label: 'Выбор только из списка' },
+    { value: 'lastChosenValuesBox', label: 'Последние выбранные значения' },
+    { value: 'additionalPromptBox', label: 'Дополнительная подсказка' }
+  ];
+
+  const PromptPropertiesValues = {
+    name: '',
+    value: '',
+    label: ''
+  };
+
   const modalContent = () => {
     return (
-      <div className={modalStyles.main}>
-        <div className={modalStyles.parameterTypeBlock}>
-          <p className={modalStyles.text}>Тип параметра</p>
-
-          <div className={modalStyles.allRadio}>
-            <Radio
-              id="newPrompt"
-              name="paramTypeRadio"
-              label="Новая подсказка"
-              value="newPrompt"
-              labelClass={modalStyles.label}
-            />
-          </div>
-          <div className={modalStyles.allRadio}>
-            <Radio
-              id="universeParam"
-              name="paramTypeRadio"
-              label="Параметр Юниверса"
-              value="universeParam"
-              labelClass={modalStyles.label}
-            />
-          </div>
-        </div>
-
-        <div className={modalStyles.promptTextBlock}>
-          <p className={modalStyles.text}>Текст подсказки:</p>
-          <input
-            className={modalStyles.input}
-            placeholder="Объект из таблицы"
-          />
-        </div>
-
-        <div className={modalStyles.allCheckBoxBlock}>
-          <div className={modalStyles.checkBox}>
-            <CheckBox
-              id="listValueBox"
-              name="listValueBox"
-              label="Подсказка со списком значений"
-              value="listValueBox"
-              labelClass={modalStyles.label}
-            />
-          </div>
-          <div className={modalStyles.checkBox}>
-            <CheckBox
-              id="onlyListChoiceBox"
-              name="onlyListChoiceBox"
-              label="Выбор только из списка"
-              value="onlyListChoiceBox"
-              labelClass={modalStyles.label}
-            />
-          </div>
-          <div className={modalStyles.checkBox}>
-            <CheckBox
-              id="lastChosenValuesBox"
-              name="lastChosenValuesBox"
-              label="Последние выбранные значения"
-              value="lastChosenValuesBox"
-              labelClass={modalStyles.label}
-            />
-          </div>
-          <div className={modalStyles.checkBox}>
-            <CheckBox
-              id="additionalPromptBox"
-              name="additionalPromptBox"
-              label="Дополнительная подсказка"
-              value="additionalPromptBox"
-              labelClass={modalStyles.label}
-            />
-          </div>
-        </div>
-
-        <div className={modalStyles.byDefaultBlock}>
-          <p className={modalStyles.text}>По умолчанию</p>
-
-          <div className={modalStyles.defaultCheckBoxButton}>
-            <div className={modalStyles.defaultCheckBox}>
-              <CheckBox
-                id="defaultValuesBox"
-                name="defaultValuesBox"
-                label="Значения по умолчанию"
-                value="defaultValuesBox"
-                labelClass={modalStyles.label}
-              />
+      <Formik initialValues={PromptPropertiesValues}>
+        <div className={modalStyles.main}>
+          <div className={modalStyles.parameterTypeBlock}>
+            <p className={modalStyles.text}>Тип параметра</p>
+            <div className={modalStyles.allRadio}>
+              {PromptValuesRadioBTNs.map(item => {
+                return (
+                  <RadioField
+                    name="PromptValuesRadioBTNs"
+                    key={item.value}
+                    label={item.label}
+                    value={item.value}
+                    labelClass={modalStyles.radioLabel}
+                    {...item}
+                  />
+                );
+              })}
             </div>
-            <Button className={modalStyles.button} buttonStyle={BUTTON.BLUE}>
-              Список
+          </div>
+
+          <div className={modalStyles.promptTextBlock}>
+            <p className={modalStyles.text}>Текст подсказки:</p>
+            <input
+              className={modalStyles.input}
+              placeholder="Объект из таблицы"
+            />
+          </div>
+
+          <div className={modalStyles.checkBox}>
+            {CheckBoxValues.map(item => {
+              return (
+                <CheckboxField
+                  id={item.value}
+                  name="promptCheckBoxValues"
+                  label={item.label}
+                  value={item.value}
+                  labelClass={modalStyles.label}
+                />
+              );
+            })}
+          </div>
+
+          <div className={modalStyles.byDefaultBlock}>
+            <p className={modalStyles.text}>По умолчанию</p>
+
+            <div className={modalStyles.defaultCheckBoxBlock}>
+              <div className={modalStyles.defaultCheckBox}>
+                <CheckboxField
+                  id="defaultValuesBox"
+                  name="defaultValuesBox"
+                  label="Значения по умолчанию"
+                  value="defaultValuesBox"
+                  labelClass={modalStyles.label}
+                />
+              </div>
+              <Button className={modalStyles.button} buttonStyle={BUTTON.BLUE}>
+                Список
+              </Button>
+            </div>
+            <div className={modalStyles.inputArrowBlock}>
+              <input className={modalStyles.input} />
+              <Arrow className={modalStyles.arrow} />
+              <div className={modalStyles.dataWindow} />
+            </div>
+          </div>
+
+          <div className={modalStyles.footerButtons}>
+            <Button
+              className={modalStyles.button}
+              buttonStyle={BUTTON.BIG_ORANGE}
+            >
+              Сохранить
+            </Button>
+            <Button
+              className={modalStyles.button}
+              buttonStyle={BUTTON.BIG_BLUE}
+              onClick={closeHandler}
+            >
+              Отмена
             </Button>
           </div>
-          <div className={modalStyles.inputArrowBlock}>
-            <input className={modalStyles.input} />
-            <Arrow className={modalStyles.arrow} />
-            <div className={modalStyles.dataWindow} />
-          </div>
         </div>
-
-        <div className={modalStyles.footerButtons}>
-          <Button
-            className={modalStyles.button}
-            buttonStyle={BUTTON.BIG_ORANGE}
-          >
-            Сохранить
-          </Button>
-          <Button className={modalStyles.button} buttonStyle={BUTTON.BIG_BLUE}>
-            Отмента
-          </Button>
-        </div>
-      </div>
+      </Formik>
     );
   };
   return (
