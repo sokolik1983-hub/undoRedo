5a79b7b9dc7afbeaff0687a799984e3d30117a5b
Igor Levin <levin.igor@bk.ru>
2022-03-29 21:18:53 +0300

    fix checkbox label position

--- src/modules/QueryPanel/PromptPropertiesLayer/index.jsx
+++ src/modules/QueryPanel/PromptPropertiesLayer/index.jsx
@@ -72,7 +72,7 @@ const PromptPropertiesLayer = ({ visible, onClose }) => {
                   name="promptCheckBoxValues"
                   label={item.label}
                   value={item.value}
-                  labelClass={modalStyles.label}
+                  wrapperClass={styles.checkBoxWrapper}
                 />
               );
             })}
