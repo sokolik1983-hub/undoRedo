d0315e94ab7f1197de2db0f31fc5b0eeea9fb525
dnichiporenko <dmitriy.nichiporenko@tern.ru>
2022-04-09 12:00:18 +0300

    TU2-136

--- src/modules/QueryPanel/ObjectsPanel/index.jsx
+++ src/modules/QueryPanel/ObjectsPanel/index.jsx
@@ -1,4 +1,5 @@
-import { useEffect } from 'react';
+/* eslint-disable */
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import ObjectsPanelHeader from './ObjectsPanelHeader/ObjectsPanelHeader';
@@ -10,21 +11,43 @@ import { getSymanticLayerData } from '../../../data/actions/universes';
 
 const ObjectsPanel = ({ symanticLayer, onToggleClick }) => {
   const dispatch = useDispatch();
+  const symLayersData = useSelector(state => state.app?.data?.symLayersData);
+  const structure = symLayersData?.data?.structure[0]?.children;
 
   useEffect(() => {
-    if (symanticLayer) dispatch(getSymanticLayerData(symanticLayer.id));
+    if (symanticLayer) dispatch(getSymanticLayerData(symanticLayer.id)); 
   }, [symanticLayer]);
 
-  const symLayersData = useSelector(state => state.app?.data?.symLayersData);
-  const structure = symLayersData?.data?.structure[0];
+  /**
+   * Собственный стейт компонента
+  */
+  const [filterName, setFilterName] = useState('');
+  const [filterId, setFilterId] = useState([]);
+
+  /**
+   * Отфильтрованный по имени и objectType_id массив списка
+  */
+  const filteredStructure = structure
+    ?.filter(item =>
+      item?.field.toLowerCase().includes(filterName?.toLowerCase())
+    )
+    .filter(item => {
+      if (!filterId.length) return true;
+      return filterId.includes(item.objectType_id);
+    });
 
   return (
     <div className={styles.root}>
       <ObjectsPanelHeader onToggleClick={onToggleClick} />
       <Divider color="#0D6CDD" />
-      <ObjectsPanelFilters />
+      <ObjectsPanelFilters
+        setFilterName={setFilterName}
+        value={filterName}
+        setFilterId={setFilterId}
+        filterId={filterId}
+      />
       <div className={styles.panelListContainer}>
-        <ObjectsPanelList rootFolder={structure} />
+        <ObjectsPanelList rootFolder={filteredStructure} />
       </div>
     </div>
   );
