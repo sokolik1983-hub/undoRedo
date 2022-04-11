e10ae9937d77534ca3786d2e58f1322745fcbed7
Ivan Prohorov <C:\Users\ivanp\AppData\Roaming\The Bat!>
2022-04-11 10:39:42 +0300

    проверка camelCase

--- src/pages/HomePage/HomePageButton/HomePageButton.jsx
+++ src/pages/HomePage/HomePageButton/HomePageButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link as RouterLink } from 'react-router-dom';
 import { Tooltip } from '@material-ui/core';
-import styles from './homePageButton.module.scss';
+import styles from './HomePageButton.module.scss';
 
 const HomePageButton = ({ title, href, icon, isDocument }) => {
   const appWordsArray = [
