65766d9977f7157a005c2fbaac9ac0ea181c6987
Ivan Prokhorov <Ivan.prohorov@tern.ru>
2022-04-27 11:08:46 +0300

    исправлено отображение списка таблиц при нажатии на крестик инпута (все таблицы отображаются)

--- src/modules/Symlayers/SchemaEditorBlock/index.jsx
+++ src/modules/Symlayers/SchemaEditorBlock/index.jsx
@@ -67,7 +67,7 @@ const ShemaEditorBlock = ({
   const onCloseInput = () => {
     setIsActive(!isActive);
     setSearchValue('');
-    setFilterableFields([])
+    setFilterableFields(selectedTableColumns)
   }
 
   return (
