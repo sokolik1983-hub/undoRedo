79e77eb8e9391f1a50b3f0ad283a4916ae616096
Азим Макаев <azim@tern.ru>
2022-05-11 12:14:04 +0300

    добавление sql попапа

---
+++ src/modules/QueryPanel/SqlPopup/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import Modal from "../../../common/components/Modal";
+import Button from "../../../common/components/Button";
+import styles from './SqlPopup.module.scss';
+
+const SqlPopup = ({ onClose }) => {
+
+  const onClickAction = () => {
+    onClose();
+  }
+
+  const content = ( 
+    <div className={styles.sql}>
+      <Button className={styles.test}>
+        Тест
+      </Button>
+      <Button className={styles.exit} onClick={onClickAction}>
+        Выйти
+      </Button>
+    </div>
+  )
+  return (
+    <div>
+      <Modal
+        title="Просмотр SQL"
+        visible
+        content={content}
+        withScroll={false}
+        titleClassName={styles.title}
+        dialogClassName={styles.dialog}
+        headerClassName={styles.header}
+        onClose={onClickAction}
+      />
+    </div>
+  );
+}
+
+export default SqlPopup;
+
+SqlPopup.propTypes = {
+  onClose: PropTypes.func
+}
