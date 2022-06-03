63c261d4ca745018ed12700497c38d6354d41a6a
dnichiporenko <dmitriy.nichiporenko@tern.ru>
2022-06-03 22:20:09 +0300

    TU2-242

--- src/pages/NotFoundPage/index.jsx
+++ src/pages/NotFoundPage/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import Button from '../../common/components/Button/index';
 import { ReactComponent as NotFound } from '../../layout/assets/notFound.svg';
 import { BUTTON } from '../../common/constants/common';
 import styles from './NotFoundPage.module.scss';
 
 const NotFoundPage = () => {
+
   return (
     <div className={styles.root}>
       <h1>Ошибка 404</h1>
@@ -13,7 +13,11 @@ const NotFoundPage = () => {
         <br />
         Стоит попробовать еще раз, но проверив запрос.
       </p>
-      <Button buttonStyle={BUTTON.BIG_BLUE}>Ок, вернуться!</Button>
+      <a href="/Universe/dashboard">
+        <Button buttonStyle={BUTTON.BIG_BLUE}>
+          Ок, вернуться!
+        </Button>
+      </a>
       <NotFound />
     </div>
   );
