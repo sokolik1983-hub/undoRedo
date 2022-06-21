4a55f97543f23bea3bd982199cd8f7d3c41dfd57
artema <aarakelyan@kameleoon.com>
2022-06-21 18:30:35 +0400

    add response params

--- src/data/helpers.js
+++ src/data/helpers.js
@@ -1,33 +1,35 @@
 /* eslint-disable camelcase */
 import axios from 'axios';
 import { setLoadingData } from './reducers/ui';
-import { SERVER_API_URL } from '../common/constants/config';
+import {
+  PENDING_RESPONSE,
+  ATTEMPTS,
+  PENDING_SERVER_TIMER,
+  SERVER_API_URL
+} from '../common/constants/config';
 // eslint-disable-next-line import/no-cycle
 import { notificationShown } from './reducers/notifications';
-
-const PENDING_SERVER_TIMER = 1000;
-const ATTEMPTS = 50;
 
 // это запрос готовности данных
 export const requestReady = async ({ id, dispatch }) => {
   const response = await axios({
-      method: 'get',
-      url: `${SERVER_API_URL}?id=${id}`,
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-    });
-    // ответ от сервера всегда будет с кодом 200, даже если прийдет ошибка
-    // разница в том, что response.data.result будет отсутствовать в ошибке
-    // вместо этого мы получим response.data.errors
-    if (response && response.status === 200) {
-      // добавить все условия
-      if (
-        response.data.result === 1 ||
-        response.data.result === 'pending'
-      ) {
-        return response.data;
-      }
+    method: 'get',
+    url: `${SERVER_API_URL}?id=${id}`,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  });
+  // ответ от сервера всегда будет с кодом 200, даже если прийдет ошибка
+  // разница в том, что response.data.result будет отсутствовать в ошибке
+  // вместо этого мы получим response.data.errors
+  if (response && response.status === 200) {
+    // добавить все условия
+    if (
+      response.data.result === 1 ||
+      response.data.result === PENDING_RESPONSE
+    ) {
+      return response.data;
+    }
 
     if (response.data.result === 0 && response.data.errors) {
       response.data.errors.forEach(item => {
@@ -93,81 +95,36 @@ export const requestReady = async ({ id, dispatch }) => {
 //   });
 // }
 
-const requesterTimeout = ({ id, dispatch }) => new Promise((resolve, reject) => {
-  let tryCount = 0
-  const interval = async () => {
-    tryCount++;
-    const response = await requestReady({
-      id,
-      dispatch
-    });
-    if (response?.result === 1 || !response) {
-      setLoadingData(false);
-      return resolve(response);
-    }
-    if (response?.result === 'failed') {
-      console.log('id запроса устарел');
-      return reject(response);
-    }
-    if (response?.result === 'pending') {
-      if (tryCount >= ATTEMPTS) {
-        console.log('исчерпаны попытки, выход из запроса');
+const requesterTimeout = ({ id, dispatch }) =>
+  new Promise((resolve, reject) => {
+    let tryCount = 0;
+    const interval = async () => {
+      tryCount++;
+      const response = await requestReady({
+        id,
+        dispatch
+      });
+      if (response?.result === 1 || !response) {
+        setLoadingData(false);
+        return resolve(response);
+      }
+      if (response?.result === 'failed') {
+        console.log('id запроса устарел');
         return reject(response);
       }
-      console.log('данные на сервере еще не готовы');
-      return setTimeout(interval, PENDING_SERVER_TIMER);
-    
-    }
-    return null;
+      if (response?.result === PENDING_RESPONSE) {
+        if (tryCount >= ATTEMPTS) {
+          console.log('исчерпаны попытки, выход из запроса');
+          return reject(response);
+        }
+        console.log('данные на сервере еще не готовы');
+        return setTimeout(interval, PENDING_SERVER_TIMER);
+      }
+      return null;
+    };
 
-  }
-
-  setTimeout(interval, PENDING_SERVER_TIMER);
-
-
-})
-
-
-
-
-
-
-//   return new Promise((resolve, reject) => {
-//     let tryCount = 0;
-//     const timer = setInterval(async () => {
-//       tryCount++;
-
-//       const response = await requestReady({
-//         id,
-//         dispatch
-//       });
-
-//       console.log('rt resp',response)
-
-//       if (response?.result === 1 || !response) {
-//         setLoadingData(false);
-//         clearInterval(timer);
-//         return resolve(response);
-//       }
-//       if (response?.result === 'failed') {
-//         console.log('id запроса устарел');
-//         clearInterval(timer);
-//         return reject(response);
-//       }
-//       if (response?.result === 'pending') {
-//         if (tryCount >= ATTEMPTS) {
-//           console.log('исчерпаны попытки, выход из запроса');
-//           clearInterval(timer);
-//           return reject(response);
-//         }
-//         console.log('данные на сервере еще не готовы');
-//         return reject(response);
-//       }
-//       return null;
-//     }, PENDING_SERVER_TIMER);
-//   });
-// }
-
+    setTimeout(interval, PENDING_SERVER_TIMER);
+  });
 
 // обычный запрос, в ответ на который мы получаем id запроса
 // для получения данных по запросу, надо отправить новый запрос с указанием id
@@ -175,7 +132,7 @@ const requesterTimeout = ({ id, dispatch }) => new Promise((resolve, reject) => 
 export const request = async ({ params, code, dispatch }) => {
   const token = localStorage.getItem('token');
   const streamreceiver = localStorage.getItem('streamreceiver');
-  
+
   try {
     const response = await axios({
       method: 'post',
@@ -193,19 +150,26 @@ export const request = async ({ params, code, dispatch }) => {
       return requesterTimeout({ id: response.data, dispatch });
     }
   } catch (err) {
-    dispatch(notificationShown({
-      message: err.message,
-      messageType: 'error',
-      reason: 'Сервер не отвечает',
-      advice: 'Обратиттесь к системному администратору' }));
+    dispatch(
+      notificationShown({
+        message: err.message,
+        messageType: 'error',
+        reason: 'Сервер не отвечает',
+        advice: 'Обратиттесь к системному администратору'
+      })
+    );
   }
   dispatch(setLoadingData(false));
   return null;
 };
 
-
 // запросы в одну сторону, на которые не ждем ответ
-export const requestWithoutResponse = async ({ params, code, token, dispatch }) => {
+export const requestWithoutResponse = async ({
+  params,
+  code,
+  token,
+  dispatch
+}) => {
   try {
     const response = await axios({
       method: 'post',
@@ -213,19 +177,21 @@ export const requestWithoutResponse = async ({ params, code, token, dispatch }) 
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       },
-      data: `code=${code}&token=${encodeURI(token) || null}&format=JSON&params=${
-        params ? JSON.stringify(params) : ''
-      }`
+      data: `code=${code}&token=${encodeURI(token) ||
+        null}&format=JSON&params=${params ? JSON.stringify(params) : ''}`
     });
     if (response && response.status === 200) {
       return null;
     }
   } catch (err) {
-    dispatch(notificationShown({
-      message: err.message,
-      messageType: 'error',
-      reason: 'Сервер не отвечает',
-      advice: 'Обратиттесь к системному администратору' }));
+    dispatch(
+      notificationShown({
+        message: err.message,
+        messageType: 'error',
+        reason: 'Сервер не отвечает',
+        advice: 'Обратиттесь к системному администратору'
+      })
+    );
   }
   return null;
 };
@@ -270,7 +236,6 @@ export const getTableIdFromParams = ({
 //   return result;
 // };
 
-
 // let t = {
 //   a: {
 //     b: {
@@ -286,17 +251,19 @@ export const getTableIdFromParams = ({
 //         },
 //       ]
 
-
 //     },
 //     g: 'ffff'
 //   },
 //   f: 2
 // }
 
-
-
-
-export const deepObjectSearch = ({ target, key, value, parentNodes = [], parentKey = null}) => {
+export const deepObjectSearch = ({
+  target,
+  key,
+  value,
+  parentNodes = [],
+  parentKey = null
+}) => {
   let result = [];
   const keys = Object.keys(target);
   for (let i = 0; i < keys.length; i++) {
@@ -304,14 +271,20 @@ export const deepObjectSearch = ({ target, key, value, parentNodes = [], parentK
 
     if (typeof target[objectKey] === 'object') {
       result = result.concat(
-        deepObjectSearch({ target: target[objectKey], key, value, parentNodes: [target, ...parentNodes], parentKey: objectKey })
+        deepObjectSearch({
+          target: target[objectKey],
+          key,
+          value,
+          parentNodes: [target, ...parentNodes],
+          parentKey: objectKey
+        })
       );
     }
     /*eslint-disable */
     if (objectKey !== key) continue;
 
-    if(objectKey === key && target[objectKey] === value) {
-      result.push({target, parentKey, parentNodes })
+    if (objectKey === key && target[objectKey] === value) {
+      result.push({ target, parentKey, parentNodes });
     }
   }
   return result;
