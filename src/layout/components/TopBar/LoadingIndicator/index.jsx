17db5490a1618ba89ead29533c91e685c5c8ed0d
dnichiporenko <dmitriy.nichiporenko@tern.ru>
2022-06-23 13:31:32 +0300

    TU2-267

--- src/layout/components/TopBar/LoadingIndicator/index.jsx
+++ src/layout/components/TopBar/LoadingIndicator/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Gears from '../../../../common/components/Gears';
 // import styles from './LoadingIndicator.module.scss';
 
 function LoadingIndicator() {
   const ui = useSelector(state => state.app.ui);
 
   return ui && ui.isLoadingData ? (
-    <div>Loading</div>
+    <Gears isSpinning />
   ) : null;
 }
 
