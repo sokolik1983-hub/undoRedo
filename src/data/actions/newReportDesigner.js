0070ed9394660e18075236147ce2a93a8142081b
artema <aarakelyan@kameleoon.com>
2022-07-03 23:23:35 +0400

    add new functions for saving report

--- src/data/actions/newReportDesigner.js
+++ src/data/actions/newReportDesigner.js
@@ -2,11 +2,6 @@
 import { ActionCreators } from 'redux-undo';
 import { request } from '../helpers';
 import { setStructure, setVariables } from '../reducers/new_reportDesigner';
-import {
-  REP_GET_ELEMENT_DATA,
-  REP_GET_REPORT_STRUCTURE,
-  REP_GET_VARIABLES
-} from './response_const';
 
 export const refreshServerResponse = queryParams => {
   return async dispatch => {
@@ -34,8 +29,6 @@ export const getStreamReceiever = queryParams => {
 
 export const getReportStructure = queryParams => {
   return async dispatch => {
-    // dispatch(setStructure(REP_GET_REPORT_STRUCTURE.structure));
-
     const response = await request({
       code: 'REP.GET_REPORT_STRUCTURE',
       params: queryParams,
@@ -43,6 +36,59 @@ export const getReportStructure = queryParams => {
     });
     if (response) {
       dispatch(setStructure(response.structure));
+    }
+  };
+};
+
+export const createReport = queryParams => {
+  return async dispatch => {
+    const response = await request({
+      code: 'REP.CREATE',
+      params: queryParams,
+      dispatch
+    });
+    if (response) {
+      console.log(response);
+      // dispatch(getReportStructure({ report_id: queryParams.report_id }));
+    }
+  };
+};
+export const saveReport = queryParams => {
+  return async dispatch => {
+    const response = await request({
+      code: 'REP.SAVE',
+      params: queryParams, // {"name" : "сложное уникальное имя", "parent_id' : 15}
+      dispatch
+    });
+    if (response) {
+      console.log(response);
+      // dispatch(getReportStructure({ report_id: queryParams.report_id }));
+    }
+  };
+};
+export const openReport = queryParams => {
+  return async dispatch => {
+    const response = await request({
+      code: 'REP.OPEN',
+      params: queryParams, // {"id": 10106}
+      dispatch
+    });
+    if (response) {
+      console.log(response);
+      // dispatch(getReportStructure({ report_id: queryParams.report_id }));
+    }
+  };
+};
+export const getReportTabs = queryParams => {
+  return async dispatch => {
+    const response = await request({
+      code: 'REP.GET_ALL_OBJECT',
+      params: queryParams, // {"id": 10106}
+      dispatch
+    });
+    if (response) {
+      console.log(response);
+      // dispatch(getReportStructure({ report_id: queryParams.report_id }));
     }
   };
 };
@@ -61,20 +107,13 @@ export const setReportStructure = queryParams => {
 };
 
 export const getElementData = (queryParams, callback) => {
-  // token:{{lastToken}}
-  // params:{"report_id": "R1", "element_id": "R1.B.2.HB" }
-  // streamreceiver:{{lastThread}}
-  // return async dispatch => {
-  //   dispatch(setStructure(REP_GET_ELEMENT_DATA.data));
-  // };
   return async dispatch => {
-
     const response = await request({
       code: 'REP.GET_ELEMENT_DATA',
       params: queryParams,
       dispatch
     });
-    
+
     if (response) {
       callback(response);
       // dispatch(setStructure(response.structure));
@@ -84,8 +123,6 @@ export const getElementData = (queryParams, callback) => {
 
 export const getVariables = () => {
   return async dispatch => {
-    // dispatch(setVariables(REP_GET_VARIABLES.variables));
-
     const response = await request({
       code: 'REP.GET_VARIABLES',
       params: {},
