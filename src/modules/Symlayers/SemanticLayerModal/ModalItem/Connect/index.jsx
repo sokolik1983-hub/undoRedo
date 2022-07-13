ee5b551cca7b7bb3d9ebc372ae5736cce1f39345
Ivan Prokhorov <Ivan.prohorov@tern.ru>
2022-07-13 12:22:43 +0300

    добавлен список коннекторов, удален пропс connectorName

--- src/modules/Symlayers/SemanticLayerModal/ModalItem/Connect/index.jsx
+++ src/modules/Symlayers/SemanticLayerModal/ModalItem/Connect/index.jsx
@@ -1,24 +1,57 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
 import Gears from '../../../../../common/components/Gears';
 import ModalItem from '..';
 import styles from './Connect.module.scss';
 import { ReactComponent as WireIcon } from '../../../../../layout/assets/semanticLayerModal/wire.svg';
 import Select from '../../../../../common/components/Select';
 import Button from '../../../../../common/components/Button';
+import {
+  getConnectorFolderChildren,
+  getConnectorsFolderId
+} from '../../../../../data/actions/connectors';
 
 /**
  * @param title - строка для заголовка
  * @param connectorName - строка с названием коннектора
  */
 
-const Connect = ({ title, connectorName }) => {
+const Connect = ({ title }) => {
+  const dispatch = useDispatch();
 
   const [isActive, setIsActive] = useState(false);
 
   const onClickAction = () => {
     setIsActive(!isActive);
-  }
+  };
+
+  useEffect(() => {
+    dispatch(getConnectorsFolderId({ folderType: 'USER_CN' }));
+  }, []);
+
+  const connectorsFolderId = useSelector(
+    state => state.app.data.connectorsFolderId
+  );
+
+  useEffect(() => {
+    dispatch(getConnectorFolderChildren({ id: connectorsFolderId }));
+  }, [connectorsFolderId]);
+
+  const connectors = useSelector(state => state.app.data.connectors);
+
+  const optionsArray = [];
+
+  const options = () =>
+    connectors?.list?.map(item => {
+      if (item.kind === 'CON') {
+        optionsArray.push({ text: item.name, value: item.name, key: item.id });
+      }
+      // eslint-disable-next-line no-useless-return, consistent-return
+      return;
+    });
+
+  options();
 
   return (
     <ModalItem title={title}>
@@ -33,15 +66,13 @@ const Connect = ({ title, connectorName }) => {
           </div>
         </div>
         <Select
-          defaultValue={connectorName || 'Соединение 01 проба'}
-          name='name1'
-          options={[{ value: '1', text: 'Соединение 02 проба' }]}
+          defaultValue={optionsArray[0]?.text || 'Получаем список…'}
+          name="name1"
+          options={optionsArray}
         />
         <div className={styles.buttonsWrapper}>
-          <Button className={styles.edit}>
-            Редактировать
-          </Button>
-          <Button type='button' onClick={onClickAction} className={styles.test}>
+          <Button className={styles.edit}>Редактировать</Button>
+          <Button type="button" onClick={onClickAction} className={styles.test}>
             Тест соедиения
           </Button>
         </div>
@@ -54,7 +85,6 @@ export default Connect;
 
 Connect.propTypes = {
   title: PropTypes.string,
-  connectorName: PropTypes.string
 };
 
 Connect.defaultProps = {
